f2d09b3b5cd560458e1d57147cb0451db75db449
root <root@fogg.me.uk>
2014-10-21 17:22:07 -0400

    Fixed PHP EOFs

--- php/meta.php
+++ php/meta.php
@@ -6,4 +6,3 @@
     <link rel="stylesheet" type="text/css" href="http://fogg.me.uk/style/main.css">
     <link rel="icon" href="http://fogg.me.uk/images/icons/flav.ico">
     <link rel="shortcut icon" href="http://fogg.me.uk/images/icons/flav.ico">
-
