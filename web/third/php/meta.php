e19519f924f6ea9228015be5f8850128050f0c9f
Joshua H. Fogg <joshua.h.fogg@gmail.com>
2015-12-20 01:27:55 +0000

    Fixed 3rd Blog Links
    Similar to before but with the 3rd design

--- web/third/php/meta.php
+++ web/third/php/meta.php
@@ -1,9 +1,9 @@
 <meta name="description" content="Simple Personal Website"> 
-    <meta name="keywords" content="Blog"> 
-    <meta name="author" content="Joshua Fogg"> 
-    <meta charset="UTF-8"> 
+    <meta name="keywords" content="Blog">
+    <meta name="author" content="Joshua Fogg">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="http://fogg.me.uk/style/main.css">
-    <link rel="icon" href="http://fogg.me.uk/images/icons/flav.ico">
-    <link rel="shortcut icon" href="http://fogg.me.uk/images/icons/flav.ico">
+    <link rel="stylesheet" type="text/css" href="http://foggalong.github.io/web/third/style/main.css">
+    <link rel="icon" href="http://foggalong.github.io/web/third/images/icons/flav.ico">
+    <link rel="shortcut icon" href="http://foggalong.github.io/web/third/images/icons/flav.ico">
     <link rel="stylesheet" href="//code.cdn.mozilla.net/fonts/fira.css">
