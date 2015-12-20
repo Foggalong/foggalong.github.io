518ad5fc8deb4c1c8469e939eaadd3de8bfd9a9e
Joshua H. Fogg <joshua.h.fogg@gmail.com>
2015-12-20 01:50:28 +0000

    Merged in PHP
    Pages doesn't seem to support PHP includes so I've just merged in the
    HTML

--- web/third/php/navbar.php
+++ web/third/php/navbar.php
@@ -1,28 +1,28 @@
 <div class="nav">
     <ul class="nav">
         <li class="nav" title="Home">
-            <a class="nav" href="http://fogg.me.uk/">
-                <img class="image" src="http://fogg.me.uk/images/nav/home.svg" alt="Home">
+            <a class="nav" href="http://foggalong.github.io/web/third/">
+                <img class="image" src="http://foggalong.github.io/web/third/images/nav/home.svg" alt="Home">
             </a>
         </li>
         <li class="nav" title="Blog">
-            <a class="nav" href="http://fogg.me.uk/blog">
-                <img class="image" src="http://fogg.me.uk/images/nav/blog.svg" alt="Blog">
+            <a class="nav" href="http://foggalong.github.io/web/third/blog">
+                <img class="image" src="http://foggalong.github.io/web/third/images/nav/blog.svg" alt="Blog">
             </a>
         </li>
         <li class="nav" title="Work">
-            <a class="nav" href="http://fogg.me.uk/work">
-                <img class="image" src="http://fogg.me.uk/images/nav/work.svg" alt="Work">
+            <a class="nav" href="http://foggalong.github.io/web/third/work">
+                <img class="image" src="http://foggalong.github.io/web/third/images/nav/work.svg" alt="Work">
             </a>
         </li>
         <li class="nav" title="About">
-            <a class="nav" href="http://fogg.me.uk/about">
-                <img class="image" src="http://fogg.me.uk/images/nav/about.svg" alt="About">
+            <a class="nav" href="http://foggalong.github.io/web/third/about">
+                <img class="image" src="http://foggalong.github.io/web/third/images/nav/about.svg" alt="About">
             </a>
         </li>
         <li class="nav-r" title="Top">
             <a class="nav" href="#">
-                <img class="image" src="http://fogg.me.uk/images/nav/top.svg" alt="Top">
+                <img class="image" src="http://foggalong.github.io/web/third/images/nav/top.svg" alt="Top">
             </a>
         </li>
     </ul>
